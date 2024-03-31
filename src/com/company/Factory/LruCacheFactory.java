535478d9ab04bead956da9456f5e8ddc4ad32770
Maria Kozlova <november00271@gmail.com>
2024-03-31 18:20:15 +0300

    changes

--- src/com/company/Factory/LruCacheFactory.java
+++
@@ -1,7 +0,0 @@
-package com.company.Factory;
-
-import com.company.LruCache.LruCache;
-
-public abstract class LruCacheFactory<K, V> {
-    public abstract LruCache<K, V> createLruCache(int limit);
-}
